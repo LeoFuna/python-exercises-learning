18fa624dfa51e2a1ea52c9218a72d270773785a5
leofuna <leo.funa@gmail.com>
2022-01-22 13:08:54 -0300

    Estudos python dia 2 primeiros exercicios pra treino

--- Entrada_Saida_De_Dados/exercicios_estudo.py
+++ Entrada_Saida_De_Dados/exercicios_estudo.py
@@ -8,14 +8,30 @@ def imprime_na_vertical(nome):
         n += 1
 
 
+import sys
+
+
 def soma_numeros():
-    dados = input('Digite os números a serem somados: ')
+    dados = input("Digite os números a serem somados: ")
     total = 0
     lista_de_numeros = dados.split()
     for numero in lista_de_numeros:
         if isdigit(numero):
             total += int(numero)
         else:
-            print(f"Erro ao somar valores, '{numero}' é um valor inválido")
+            print(
+                f"Erro ao somar valores, '{numero}' é um valor inválido",
+                file=sys.stderr,
+            )
     return total
 
+
+def anota_alunos_aprovados():
+    notas = open("notas.txt", mode="r")
+    file = open("file.txt", mode="w")
+    for line in notas:
+        aluno_com_nota = line.split()
+        if int(aluno_com_nota[1]) >= 6:
+            file.write(f"{aluno_com_nota[0]}\n")
+    file.close()
+    notas.close()
