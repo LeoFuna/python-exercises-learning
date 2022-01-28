956b7b5a558dbd4eff0a9db1ade6496d09d2810c
leofuna <leo.funa@gmail.com>
2022-01-28 18:45:28 -0300

    Exercicio 2

--- POO/exercicios_do_dia.py
+++ POO/exercicios_do_dia.py
@@ -25,3 +25,31 @@ class Tv():
             self.__ligada = False
         else:
             self.__ligada = True
+
+
+# Exercicio 2
+class Estatistica:
+
+    @classmethod
+    def media(cls, lista_numeros):
+        return sum(lista_numeros) / len(lista_numeros)
+
+    @classmethod
+    def mediana(cls, lista_numeros):
+        tamanho_lista = len(lista_numeros)
+        numeros_ordenados = sorted(lista_numeros)
+        if tamanho_lista % 2 == 0:
+            return (numeros_ordenados[int((tamanho_lista / 2)) - 1] + numeros_ordenados[int(tamanho_lista / 2)]) / 2
+        
+        return numeros_ordenados[tamanho_lista // 2]
+
+    @classmethod
+    def moda(cls, lista_numeros):
+        dicionario = {}
+        for numero in lista_numeros:
+            if numero in dicionario.keys():
+                dicionario[numero] += 1
+            else:
+                dicionario[numero] = 1
+        return max(dicionario, key=dicionario.get)
+
