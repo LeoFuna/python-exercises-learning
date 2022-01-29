74330275e64ec142bb8b866014160b3713c91081
leofuna <leo.funa@gmail.com>
2022-01-29 09:04:53 -0300

    Exercicio 4

--- POO/exercicios_do_dia.py
+++ POO/exercicios_do_dia.py
@@ -1,8 +1,5 @@
 # Exercicio 1
-from abc import abstractmethod
-
-
-class Tv():
+class Tv:
     def __init__(self, tamanho):
         self.__volume = 50
         self.__canal = 1
@@ -12,17 +9,17 @@ class Tv():
     def aumentar_volume(self):
         if self.__volume < 99:
             self.__volume += 1
-    
+
     def diminiuir_volume(self):
         if self.__volume > 0:
             self.__volume -= 1
-    
+
     def modificar_canal(self, canal):
         if 0 < canal < 100:
-            self.__canal  = canal
+            self.__canal = canal
         else:
             raise ValueError("Canal Indisponível")
-    
+
     def ligar_desligar(self):
         if self.__ligada:
             self.__ligada = False
@@ -32,7 +29,6 @@ class Tv():
 
 # Exercicio 2
 class Estatistica:
-
     @classmethod
     def media(cls, lista_numeros):
         return sum(lista_numeros) / len(lista_numeros)
@@ -42,8 +38,11 @@ class Estatistica:
         tamanho_lista = len(lista_numeros)
         numeros_ordenados = sorted(lista_numeros)
         if tamanho_lista % 2 == 0:
-            return (numeros_ordenados[int((tamanho_lista / 2)) - 1] + numeros_ordenados[int(tamanho_lista / 2)]) / 2
-        
+            return (
+                numeros_ordenados[int((tamanho_lista / 2)) - 1]
+                + numeros_ordenados[int(tamanho_lista / 2)]
+            ) / 2
+
         return numeros_ordenados[tamanho_lista // 2]
 
     @classmethod
@@ -56,11 +55,12 @@ class Estatistica:
                 dicionario[numero] = 1
         return max(dicionario, key=dicionario.get)
 
-#Exercicio 3
+
+# Exercicio 3
 from abc import ABC, abstractmethod
 
+
 class FiguraGeometrica(ABC):
-    
     @abstractmethod
     def area(self):
         raise NotImplementedError
@@ -68,6 +68,7 @@ class FiguraGeometrica(ABC):
     @abstractmethod
     def perimetro(self):
         raise NotImplementedError
+
 
 class Quadrado(FiguraGeometrica):
     def __init__(self, lado):
@@ -79,24 +80,75 @@ class Quadrado(FiguraGeometrica):
     def perimetro(self):
         return self.lado * 4
 
+
 class Retangulo(FiguraGeometrica):
     def __init__(self, base, altura):
         self.base = base
         self.altura = altura
-    
+
     def area(self):
         return self.altura + self.base
-    
+
     def perimetro(self):
         return self.altura * 2 + self.base * 2
+
 
 class Circulo(FiguraGeometrica):
     def __init__(self, raio):
         self.raio = raio
-    
+
     def area(self):
-        return 3.14 * (self.raio **2)
+        return 3.14 * (self.raio ** 2)
 
     def perimetro(self):
         return 2 * 3.14 * self.raio
 
+
+# Exercicio 4
+# from abc import ABC
+class ManipuladorDeLog(ABC):
+    @classmethod
+    @abstractmethod
+    def log(cls, msg):
+        raise NotImplementedError
+
+
+class LogEmArquivo(ManipuladorDeLog):
+    @classmethod
+    def log(cls, msg):
+        with open("log.txt", "a") as arquivo:
+            print(msg, file=arquivo)
+
+
+class LogEmTela(ManipuladorDeLog):
+    @classmethod
+    def log(cls, msg):
+        print(msg)
+        pass
+
+
+class Log:
+    def __init__(self, manipuladores):
+        self.__manipuladores = set(manipuladores)
+
+    def adicionar_manipulador(self, manipulador):
+        self.__manipuladores.add(manipulador)
+
+    def info(self, msg):
+        self.__log("INFO", msg)
+
+    def alerta(self, msg):
+        self.__log("ALERTA", msg)
+
+    def erro(self, msg):
+        self.__log("ERRO", msg)
+
+    def __log(self, nivel, msg):
+        for manipulador in self.__manipuladores:
+            manipulador.log(self.__formatar(nivel, msg))
+
+    def __formatar(self, nivel, msg):
+        return f"{nivel}: {msg}"
+
+
+# Exercicio 5
